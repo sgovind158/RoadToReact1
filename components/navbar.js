ceae27c2d14dac3a2cf33bdff5227e0cc0558aff
sgovind158 <govindsahu2842@gmail.com>
2022-04-30 19:02:34 +0530

    road to react ass1

---
+++ components/navbar.js
@@ -0,0 +1,37 @@
+function navbar(){
+
+    return `
+    <h3>
+    <a href="/">Home</a>
+    </h3>
+    <div id="option" >
+    
+    <h3>
+    <a href="/jevelory.html">Jevelary</a>
+    
+    </h3>
+    
+    <h3>
+    <a href="/electronic.html">Electronic</a>
+    
+    </h3>
+    
+    <h3>
+    <a href="login.html">Login</a>
+    
+    </h3>
+    
+    <h3>
+    <a href="signup.html">Signup</a>
+    
+    </h3>
+    
+    
+    
+    </div>
+   
+    
+    
+    `;}
+
+    export {navbar} ;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {navbar} from "../components/navbar";
+import "../styles/navbar.css";
+
+// console.log(navbar)
+
+let navBar = document.getElementById("navbar")
+navBar.innerHTML =  navbar();
